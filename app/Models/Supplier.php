7505b8f87851a3edd9e4be8ab10a892de17e3902
AniKhairiyah <anikhairiyah23@mhs.politala.ac.id>
2024-11-05 00:04:32 +0700

    Update Supplier.php

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory;
+
     protected $primaryKey = 'id_supplier';
     public $incrementing = false; // Custom primary key (string)
 
