ba0bde0a6174a0e3d362c40f40fa62859cb43885
sitirukmana05 <sitirukmana23@mhs.politala.ac.id>
2024-11-08 15:48:07 +0800

    Create Pengguna.php

---
+++ app/Models/Pengguna.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengguna extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_pengguna';
+    
+    // Jika 'id_pengguna' adalah string, tambahkan juga
+    protected $keyType = 'string';
+    public $incrementing = false; // Jika primary key tidak auto-increment
+    protected $table = 'penggunas'; // pastikan ini sesuai dengan nama tabel di database
+    protected $fillable = [
+        'id_pengguna',
+        'nama_pengguna',
+        'no_telepon',
+        'username',
+        'password',
+    ];
+}
