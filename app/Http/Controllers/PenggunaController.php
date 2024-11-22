63db83c9725d7e922a784c57f9cea0908a23ee50
AniKhairiyah <anikhairiyah23@mhs.politala.ac.id>
2024-11-22 12:23:34 +0700

    memperbaiki pengguna controller

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pengguna;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Pengguna;
+use Illuminate\Support\Facades\Hash;
 
 class PenggunaController extends Controller
 {
@@ -83,5 +85,46 @@ class PenggunaController extends Controller
         $pengguna->delete(); // Menghapus pengguna
 
         return redirect()->route('pengguna.index')->with('success', 'Pengguna berhasil dihapus.'); // Redirect dengan pesan sukses
+    }
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    // Proses login
+    public function login(Request $request)
+    {
+        // Validasi input
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:10'],
+        ]);
+
+        // Cek kredensial dan login
+        if (Auth::attempt($credentials)) {
+            return redirect('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email yang anda masukkan salah.',
+        ]);
+    }
+
+    // Proses logout
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+
+    // Menampilkan halaman dashboard
+    public function dashboard()
+    {
+        // Pastikan bahwa hanya user yang sudah login yang bisa mengakses dashboard
+        $user = Auth::user();
+        return view('dashboard', [
+            'username' => $user->username,
+            'role' => $user->role,
+        ]);
     }
 }
