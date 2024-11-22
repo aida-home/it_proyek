9fd88d1c75c6071be526715254e43d0842635bdb
AniKhairiyah <anikhairiyah23@mhs.politala.ac.id>
2024-11-22 12:49:17 +0700

    memperbaiki user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,19 +3,21 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Pengguna; // Menggunakan model Pengguna
+use App\Models\Pengguna;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Menambahkan user default untuk admin
-        Pengguna::create([ // Menggunakan model Pengguna
-            'id_pengguna' => 'admin001', // Menetapkan ID unik jika id_pengguna bukan auto-increment
-            'nama_pengguna' => 'Administrator', // Nama pengguna
-            'no_telepon' => '08123456789', // Nomor telepon (bisa dikosongkan atau disesuaikan)
-            'username' => 'Admin', // Username
-            'password' => bcrypt('admin123'), // Password yang sudah dienkripsi
-        ]);
+        // Cek apakah admin sudah ada
+        if (!Pengguna::where('username', 'Admin')->exists()) {
+            Pengguna::create([
+                'username' => 'Admin',
+                'no_telepon' => '08123456789',
+                'password' => Hash::make('admin123'),
+            ]);
+        }
     }
 }
+
