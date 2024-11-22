a8d0de4614ca18eb666513b76d1734689e05ee06
AniKhairiyah <anikhairiyah23@mhs.politala.ac.id>
2024-11-22 12:23:16 +0700

    memperbaiki userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,18 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\Models\Pengguna; // Menggunakan model Pengguna
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
         // Menambahkan user default untuk admin
-        User::create([
-            'username' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
+        Pengguna::create([ // Menggunakan model Pengguna
+            'id_pengguna' => 'admin001', // Menetapkan ID unik jika id_pengguna bukan auto-increment
+            'nama_pengguna' => 'Administrator', // Nama pengguna
+            'no_telepon' => '08123456789', // Nomor telepon (bisa dikosongkan atau disesuaikan)
+            'username' => 'Admin', // Username
+            'password' => bcrypt('admin123'), // Password yang sudah dienkripsi
         ]);
     }
 }
