ca5639c194d50a0063aade6be58b1507ef22286e
AniKhairiyah <anikhairiyah23@mhs.politala.ac.id>
2024-11-25 11:28:39 +0700

    memperbaiki router supplier di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -64,7 +64,7 @@
                 </div>
                 <div class="icon"></div>
             </div>
-            <a href="/Supplier" class="more-info">More info</a>
+            <a href="/Suppliers" class="more-info">More info</a>
         </div>
         <div class="card">
             <div class="card-content">
