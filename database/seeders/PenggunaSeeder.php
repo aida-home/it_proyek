1da50d2e50994b321b807df8e4544ce7ca37d1c9
aida-home <aidasekarningrum23@mhs.politala.ac.id>
2024-12-16 11:41:35 +0800

    seeder

--- database/seeders/PenggunaSeeder.php
+++ database/seeders/PenggunaSeeder.php
@@ -25,7 +25,7 @@ class PenggunaSeeder extends Seeder
             'username' => 'sayaadmin',
             'nama_pengguna' => 'Administrator',
             'no_telepon' => '08123456789',
-            'password' => Hash::make('12345678'),
+            'password' => bcrypt('12345678'),
         ]);
     }
 }
