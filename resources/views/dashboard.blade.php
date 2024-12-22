deb4e6c29913ca671f01a66bb819c211d8004430
aida-home <aidasekarningrum23@mhs.politala.ac.id>
2024-12-22 21:43:52 +0800

    Perubahan font Montserrat menjadi Verdana

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/font/css/all.min.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Dashboard</title>
